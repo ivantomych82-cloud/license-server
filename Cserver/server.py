3033a1cdfa0512e171ed9f2609434b020c2d5029
ivantomych82-cloud <ivantomych82@gmail.com>
2026-05-29 20:27:40 +0200

    Add files via upload

--- Cserver/server.py
+++ Cserver/server.py
@@ -46,5 +46,10 @@ def check():
 
     return jsonify({"status": "ok"})
 
+@app.route("/show-me-file")
+def show_file():
+    with open("licenses.json", "r") as f:
+        return f.read(), 200, {'Content-Type': 'application/json'}
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
